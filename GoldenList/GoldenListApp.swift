9a039e79592ac5ffbaeb9523e408b08fe4e85284
Maciej Sołoducha <biuro@goldenapp.pl>
2023-07-10 14:38:31 +0200

    Init mvvm pattern and NavigationView

--- GoldenList/GoldenListApp.swift
+++ GoldenList/GoldenListApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct GoldenListApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ListView()
+            }
         }
     }
 }
---
+++ GoldenList/View/ListView.swift
@@ -0,0 +1,22 @@
+//
+//  ListView.swift
+//  GoldenList
+//
+//  Created by Maciej Sołoducha on 10/07/2023.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ListView()
+        }
+    }
+}
