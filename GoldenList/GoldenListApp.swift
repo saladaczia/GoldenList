3c8e2dcf44b92e88cb19894edc9fe222bf42c566
Maciej Sołoducha <biuro@goldenapp.pl>
2023-07-10 14:16:54 +0200

    Initial Commit

---
+++ GoldenList/GoldenListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoldenListApp.swift
+//  GoldenList
+//
+//  Created by Maciej Sołoducha on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct GoldenListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
